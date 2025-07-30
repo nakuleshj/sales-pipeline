e516db10cd35731f8ad782f1e32cfa07854110c6
Nakulesh J. <nakuleshj1998@gmail.com>
2025-07-30 07:27:55 -0400

    Fix SQL Syntax Errors

--- sales_db/init_salesdb.sql
+++ sales_db/init_salesdb.sql
@@ -1,3 +1,16 @@
+CREATE TABLE IF NOT EXISTS dim_customer (
+    customer_id VARCHAR(20) PRIMARY KEY,
+    customer_name VARCHAR(100) NOT NULL,
+    country VARCHAR(50)
+);
+CREATE TABLE IF NOT EXISTS dim_product (
+    product_id VARCHAR(20) PRIMARY KEY,
+    product_name VARCHAR(100) NOT NULL
+);
+CREATE TABLE IF NOT EXISTS dim_invoice (
+    invoice_id VARCHAR(50) PRIMARY KEY,
+    sales_channel VARCHAR(50) NOT NULL
+);
 CREATE TABLE IF NOT EXISTS fact_product_sales (
     transaction_id SERIAL PRIMARY KEY,
     event_time TIMESTAMP NOT NULL,
@@ -7,21 +20,7 @@ CREATE TABLE IF NOT EXISTS fact_product_sales (
     quantity INTEGER NOT NULL,
     price NUMERIC(10, 2) NOT NULL,
     is_returned BOOLEAN NOT NULL DEFAULT FALSE,
-    FOREIGN KEY (timestamp_id) REFERENCES dim_timestamp(timestamp_id),
     FOREIGN KEY (customer_id) REFERENCES dim_customer(customer_id),
-    FOREIGN KEY (product_id) REFERENCES dim_product(product_id)
+    FOREIGN KEY (product_id) REFERENCES dim_product(product_id),
     FOREIGN KEY (invoice_id) REFERENCES dim_invoice(invoice_id)
 );
-CREATE TABLE IF NOT EXISTS dim_customer (
-    customer_id VARCHAR(20) PRIMARY KEY,
-    customer_name VARCHAR(100) NOT NULL,
-    country VARCHAR(50),
-);
-CREATE TABLE IF NOT EXISTS dim_product (
-    product_id VARCHAR(20) PRIMARY KEY,
-    product_name VARCHAR(100) NOT NULL,
-);
-CREATE TABLE IF NOT EXISTS dim_invoice (
-    invoice_id VARCHAR(50) PRIMARY KEY,
-    sales_channel VARCHAR(50) NOT NULL,
-    total_amount NUMERIC(10, 2) NOT NULL);
