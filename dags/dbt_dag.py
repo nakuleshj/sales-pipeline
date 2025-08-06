8ae462d4903585d60a059352c22c36a62df7365b
Nakulesh J. <nakuleshj1998@gmail.com>
2025-08-06 07:22:28 -0400

    Add DAG to automate dbt runs

---
+++ dags/dbt_dag.py
@@ -0,0 +1,30 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime, timedelta
+
+DBT_DIR = "/opt/airflow/dbt"
+
+default_args = {
+    'start_date': datetime(2025, 1, 1),
+}
+
+with DAG(
+    dag_id='run_dbt_pipeline',
+    schedule_interval=timedelta(minutes=1),
+    default_args=default_args,
+    catchup=False,
+) as dag:
+
+    dbt_run = BashOperator(
+        task_id='dbt_run',
+        bash_command=f"cd {DBT_DIR} && dbt run"
+    )
+
+    """
+    dbt_test = BashOperator(
+        task_id='dbt_test',
+        bash_command=f"cd {DBT_DIR} && dbt test"
+    )
+    """
+
+    dbt_run
